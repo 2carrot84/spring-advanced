0297d63845c772219c004537fa9713656c4ea3ad
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-09-07 14:32:30 +0900

    feat: 재시도 출력 AOP

--- aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -2,6 +2,7 @@ package hello.aop.exam;
 
 import org.springframework.stereotype.Repository;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 
 @Repository
@@ -9,6 +10,7 @@ public class ExamRepository {
 	private static int seq = 0;
 
 	@Trace
+	@Retry(4)
 	public String save(String itemId) {
 		seq++;
 		if (seq % 5 == 0) {
--- aop/src/test/java/hello/aop/exam/ExamTest.java
+++ aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -5,12 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 public class ExamTest {
 	@Autowired
 	ExamService examService;
